b48c67c9b35550e26307c862cf8651db916f60a3
cacaciw <tarissa.mutia@gmail.com>
2024-09-17 18:38:50 +0700

    Configured Trusted Origins

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from main.forms import GiggleForm
 from main.models import GiggleCatalogue
+from django.http import HttpResponse
+from django.core import serializers
 
 
 # Create your views here.
@@ -24,3 +26,22 @@ def create_giggle_entry(request):
 
     context = {'form': form}
     return render(request, "create_giggle_entry.html", context)
+
+def show_xml(request):
+    data = GiggleCatalogue.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = GiggleCatalogue.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = GiggleCatalogue.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = GiggleCatalogue.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+
