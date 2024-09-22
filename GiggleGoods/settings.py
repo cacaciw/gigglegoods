736a4e6b05bd12b7de6e23663d5b625854173f7f
cacaciw <tarissa.mutia@gmail.com>
2024-09-22 22:08:00 +0700

    Adding templates and configuration for login page

--- GiggleGoods/settings.py
+++ GiggleGoods/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3w3a$l7f#n4m2o5sn(4cv@du2n=^+s6d(d*77auk@u-p-$1p^i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "tarissa-mutia-gigglegoods.pbp.cs.ui.ac.id"]
 
--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
 # Create your models here.
 class GiggleCatalogue(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     name = models.CharField(max_length=255)
     price = models.IntegerField()
