f8775969e5d9d9a36f96149ac656bf9732f82da0
cacaciw <tarissa.mutia@gmail.com>
2024-10-08 18:30:52 +0700

    Added strip tags to clean new dat

--- main/forms.py
+++ main/forms.py
@@ -12,5 +12,13 @@ class GiggleForm(ModelForm):
             return strip_tags(name)
 
         def clean_price(self):
+            price = self.cleaned_data["price"]
+            return strip_tags(price)
+        
+        def clean_desc(self):
             description = self.cleaned_data["description"]
             return strip_tags(description)
+        
+        def clean_giggle(self):
+            giggleLevel = self.cleaned_data["giggleLevel"]
+            return strip_tags(giggleLevel)
